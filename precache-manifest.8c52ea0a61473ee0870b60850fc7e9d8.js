779a24d79f5e1eb42fb383163b1506da3c76875b
ammyy9908 <sb78639@gmail.com>
2020-08-13 23:04:55 +0530

    Updates

---
+++ precache-manifest.8c52ea0a61473ee0870b60850fc7e9d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5b355a627e307bbec37c11a37cabf1",
+    "url": "/quotable_api/index.html"
+  },
+  {
+    "revision": "36fa760ebb22a36c0bdc",
+    "url": "/quotable_api/static/js/2.2d013bd1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quotable_api/static/js/2.2d013bd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cda94dffa356ac730743",
+    "url": "/quotable_api/static/js/main.0d3ed5ff.chunk.js"
+  },
+  {
+    "revision": "45761416e87fbe3cef73",
+    "url": "/quotable_api/static/js/runtime-main.f3db2584.js"
+  }
+]);
